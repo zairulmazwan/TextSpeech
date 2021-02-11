4e9bb700cda8b31d140c4b1543912a9e0d2b243d
Zairul Mazwan <zairulmazwan@gmail.com>
2021-02-11 09:09:49 +0000

    Add project files.

---
+++ TextSpeech/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Speech.Synthesis;
+
+namespace TextSpeech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var synthesizer = new SpeechSynthesizer();
+            synthesizer.SetOutputToDefaultAudioDevice();
+            synthesizer.Speak("Hi Zairul, all we need to do is to make sure we keep talking");
+        }
+    }
+}
